cf3aea8c03a98a030c92fd8cd1eac168607b80d8
Fabrice Cheng <fabrice.cheng@gmail.com>
2013-01-10 17:27:02 +0100

    Initialization for myblog project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '4e9d16957852a884956f95a5abe6a1bfe375918908be5a7cf0844dec2bd84021a8c4b5b48e0686e77922eeb07bd49077073cf8be289492cf193ecd71b850aae2'
